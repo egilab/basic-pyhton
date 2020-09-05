86e02865ca5188ce125b03e7b2bdb573330dbd1c
egi <egigusniawanpradana@gmail.com>
2020-09-05 23:25:51 +0700

    update code again

--- operator.py
+++ operator.py
@@ -1,2 +1,12 @@
-for i in(range(1,1000000)):
-	print(i)
+value1 = 4
+value2 = 3
+ 
+
+print(value1 + value2)
+print(value2 - value1)
+print(value2 ** value1) #pangkat
+print(value1 / value2)
+print(value1 // value2) #hasil integer
+print('----------')
+print(20 / 3)
+print(29 % 3) #sisa hasil pembagian, 
